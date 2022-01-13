52a4f304860acff9edd922cf898600e3ebd1ddf1
Tiago Cavaco <tiago.cavaco@gmail.com>
2022-01-13 01:31:57 +0000

    chore: repo files update

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     test: {
-      plugins: ['@babel/plugin-transform-modules-commonjs', '@babel/plugin-proposal-optional-chaining'],
+      plugins: ['@babel/plugin-transform-modules-commonjs', '@babel/plugin-proposal-optional-chaining', '@babel/plugin-proposal-class-properties'],
     },
   },
 };
